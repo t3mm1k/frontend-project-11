3d5cfc07888dfdcfdaa9493b07d775ccc8cf2a30
t3mm1k <t3mm1k@gmail.com>
2026-04-24 00:39:22 +0700

    fix: парсер rss

--- src/utils/parseRssFeed.js
+++ src/utils/parseRssFeed.js
@@ -1,19 +1,34 @@
 export const parseRssFeed = (rssText) => {
   const parser = new DOMParser();
-  const feed = parser.parseFromString(rssText, 'application/xml');
-  const parserError = feed.querySelector('parsererror');
+  const xmlDocument = parser.parseFromString(rssText, 'application/xml');
+  const parserError = xmlDocument.querySelector('parsererror');
 
   if (parserError) {
     throw new Error('rss.invalid');
   }
 
-  const items = feed.querySelectorAll('item');
-  console.log('items',items);
+  const channel = xmlDocument.querySelector('channel');
 
-  items.forEach((el) => {
+  if (!channel) {
+    throw new Error('rss.invalid');
+  }
+
+  const items = Array.from(channel.querySelectorAll('item'));
+
+  const posts = items.map((el) => {
     const title = el.querySelector('title')?.textContent;
     const link = el.querySelector('link')?.textContent;
 
-    console.log(`Title: ${title}, Link: ${link}`);
+    return { title, link };
   });
+
+  const feed = {
+    title: channel.querySelector('title')?.textContent,
+    link: channel.querySelector('link')?.textContent,
+  };
+
+  return {
+    posts,
+    feed,
+  }
 }
