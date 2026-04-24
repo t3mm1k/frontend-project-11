46527ab2e68322d9f08559a2cfd264d6ae7c8b8c
t3mm1k <t3mm1k@gmail.com>
2026-04-24 14:36:57 +0700

    feat: Контроллер состояния модалки

---
+++ src/controllers/createModalController.js
@@ -0,0 +1,10 @@
+export const createModalController = (view, state) => {
+  view.onPostPreview((postId) => {
+    state.ui.viewedPosts.push(postId)
+    state.ui.modalPostId = postId;
+  });
+
+  view.onModalClose(() => {
+    state.ui.modalPostId = null;
+  });
+};
